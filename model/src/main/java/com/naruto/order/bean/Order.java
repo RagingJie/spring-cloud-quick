0da0d5f334ea9e0c3c63a4f63683c7e99dd08216
Naruto <2045933252@qq.com>
2025-03-25 15:16:08 +0800

    SpringCloud--【nacos】--订单实体类

--- model/src/main/java/com/naruto/order/bean/Order.java
+++ model/src/main/java/com/naruto/order/bean/Order.java
@@ -1,10 +1,13 @@
 package com.naruto.order.bean;
 
+import com.naruto.product.bean.Product;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import java.io.Serializable;
+import java.math.BigDecimal;
+import java.util.List;
 
 @Data
 @AllArgsConstructor
@@ -13,5 +16,10 @@ public class Order implements Serializable {
 
     private static final long serialVersionUID = 23189784902347589L;
 
-    private Long orderId;
+    private Long id;
+    private BigDecimal totalAmount;
+    private Long userId;
+    private String nickName;
+    private String address;
+    private List<Product> productList;
 }

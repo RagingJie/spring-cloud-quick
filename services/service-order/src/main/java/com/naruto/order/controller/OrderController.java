bde99ef3790f77ef3e79471639f12c740ca3f7d8
Naruto <2045933252@qq.com>
2025-03-26 10:41:14 +0800

    SpringCloud--【nacos】--订单获取配置信息

--- services/service-order/src/main/java/com/naruto/order/controller/OrderController.java
+++ services/service-order/src/main/java/com/naruto/order/controller/OrderController.java
@@ -1,9 +1,13 @@
 package com.naruto.order.controller;
 
 import com.naruto.order.bean.Order;
+import com.naruto.order.properties.OrderProperties;
+import com.naruto.order.service.OrderService;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -11,11 +15,21 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping(path = "/service-order")
 public class OrderController {
 
+    @Autowired
+    private OrderService orderService;
+
+    @Autowired
+    private OrderProperties orderProperties;
 
     @GetMapping(path = "/create")
-    public Order create() {
-        Order order = new Order();
-        order.setOrderId(1L);
-        return order;
+    public Order create(@RequestParam("userId") Long userId,
+                        @RequestParam("productId") Long productId) {
+        return orderService.create(userId, productId);
+    }
+
+
+    @GetMapping(path = "/getConfig")
+    public String getConfig() {
+        return "timeout:" + orderProperties.getTimeout() + ";  autoConfirm:" + orderProperties.getAutoConfirm() + ";  apiUrl:<a>" + orderProperties.getApiUrl() + "</a>";
     }
 }
